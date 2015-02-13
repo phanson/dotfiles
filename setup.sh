853864c758ac6504550a32b21e62a2397a90ab10
Philip Hanson <philip@cy9.org>
2015-02-13 14:05:13 -0800

    Update setup.sh with .gitignore_global symlink

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 ln -s $(pwd)/emacs ~/.emacs
 ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/gitignore_global ~/.gitignore_global
 
 mkdir -p ~/.config/terminator
 ln -s $(pwd)/terminator ~/.config/terminator/config
